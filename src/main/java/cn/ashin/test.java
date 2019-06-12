d552bd91be01a3d6469c086806f86ea0b2b92afb
130576 <kaiyuan.net>
2019-06-12 13:39:31 +0800

    test

---
+++ src/main/java/cn/ashin/test.java
@@ -0,0 +1,13 @@
+package cn.ashin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: admin
+ * Date: 2019/6/12
+ * Time: 13:38
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
