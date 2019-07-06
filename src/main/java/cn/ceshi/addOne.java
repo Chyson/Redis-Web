689056839a94c2b5705997dd44c4ad55c9f75166
LiChuanxin <lichuaxin@huaxiafinance.com>
2019-07-06 09:58:30 +0800

    update

---
+++ src/main/java/cn/ceshi/addOne.java
@@ -0,0 +1,15 @@
+package cn.ceshi;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Chyson
+ * Date: 19/7/6
+ * Time: 9:57
+ */
+public class addOne {
+    public static void main(String[] args){
+        Date date=new Date();
+    }
+}
