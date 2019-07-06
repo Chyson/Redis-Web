7dea6fe776943176c02f9af08483ed94fecfd6e3
LiChuanxin <lichuaxin@huaxiafinance.com>
2019-07-06 09:49:07 +0800

    update

---
+++ src/main/java/cn/ceshi/tings.java
@@ -0,0 +1,14 @@
+package cn.ceshi;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Chyson
+ * Date: 19/7/6
+ * Time: 9:48
+ */
+public class tings {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(i);
+    }
+}
